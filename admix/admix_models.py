3668694f691db3c27951fac8fadc9611159b054d
stevenliuyi <stevenliuyi@gmail.com>
2017-05-02 19:31:48 -0400

    add models K36, EUtest13, Jtest14 from Eurogenes project

--- admix/admix_models.py
+++ admix/admix_models.py
@@ -8,7 +8,10 @@ def models():
             'world9',
             'Eurasia7',
             'Africa9',
-            'weac2']
+            'weac2',
+            'K36',
+            'EUtest13',
+            'Jtest14']
 
 # population names for all models
 def populations(model):
@@ -106,6 +109,72 @@ def populations(model):
                 ('Sub Saharan','撒哈拉以南非洲'),
                 ('South Asian','南亚'),
                 ('Southeast Asian','东南亚')]
+    elif model == 'K36':
+        return [('Amerindian','美洲印第安'),
+                ('Arabian','阿拉伯'),
+                ('Armenian','亚美尼亚'),
+                ('Basque','巴斯克'),
+                ('Central African','中非'),
+                ('Central Euro','中欧'),
+                ('East African','东非'),
+                ('East Asian','东亚'),
+                ('East Balkan','东巴尔干'),
+                ('East Central Asian','中东亚'),
+                ('East Central Euro','中东欧'),
+                ('East Med','东地中海'),
+                ('Eastern Euro','东欧'),
+                ('Fennoscandian','芬诺斯坎底亚'),
+                ('French','法国'),
+                ('Iberian','伊比利亚'),
+                ('Indo-Chinese','印度支那'),
+                ('Italian','意大利'),
+                ('Malayan','马来亚'),
+                ('Near Eastern','近东'),
+                ('North African','北非'),
+                ('North Atlantic','北大西洋'),
+                ('North Caucasian','北高加索'),
+                ('North Sea','北海'),
+                ('Northeast African','东北非'),
+                ('Oceanian','大洋洲'),
+                ('Omotic','奥摩人'),
+                ('Pygmy','俾格米人'),
+                ('Siberian','西伯利亚'),
+                ('South Asian','南亚'),
+                ('South Central Asian','中南亚'),
+                ('South Chinese','华南'),
+                ('Volga-Ural','伏尔加－乌拉尔'),
+                ('West African','西非'),
+                ('West Caucasian','西高加索'),
+                ('West Med','西地中海')]
+    elif model == 'EUtest13':
+        return [('South Baltic','南波罗的海'),
+                ('East Euro','东欧'),
+                ('North-Central Euro','中北欧'),
+                ('Atlantic','大西洋'),
+                ('West Med','西地中海'),
+                ('East Med','东地中海'),
+                ('West Asian','西亚'),
+                ('Middle Eastern','中东'),
+                ('South Asian','南亚'),
+                ('East African','东非'),
+                ('East Asian','东亚'),
+                ('Siberian','西伯利亚'),
+                ('West African','西非')]
+    elif model == 'Jtest14':
+        return [('South Baltic','南波罗的海'),
+                ('East Euro','东欧'),
+                ('North-Central Euro','中北欧'),
+                ('Atlantic','大西洋'),
+                ('West Med','西地中海'),
+                ('Ashkenazim','德系犹太人'),
+                ('East Med','东地中海'),
+                ('West Asian','西亚'),
+                ('Middle Eastern','中东'),
+                ('South Asian','南亚'),
+                ('East African','东非'),
+                ('East Asian','东亚'),
+                ('Siberian','西伯利亚'),
+                ('West African','西非')]
     else:
         print('Model does not exist!')
         return None
