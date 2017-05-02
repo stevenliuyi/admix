71eea09db3f33db565f590c797ae7dc226905b48
stevenliuyi <stevenliuyi@gmail.com>
2017-05-02 18:29:46 -0400

    add Chinese translations

--- admix/admix.py
+++ admix/admix.py
@@ -17,20 +17,30 @@ def arguments():
                         const = 'admixture_results.txt',
                         help = 'save results as a file')
 
+    # population description in Chinese
+    parser.add_argument('-z', '--zh',
+                        action = 'store_true',
+                        help = 'display population names in Chinese')
+
     return parser.parse_args()
 
 # print out admixure analysis results
-def admix_results(models, output_filename, raw_data_format, raw_data_file=None):
+def admix_results(models, output_filename, zh, raw_data_format, raw_data_file=None):
     # write results to a file
     if (output_filename is not None):
         f = open(output_filename, 'w')
 
     for model in models:
-        result = 'Model: ' + model + '\n'
+        result = model + '\n'
         admix_frac = admix_fraction(model, raw_data_format, raw_data_file)
         populations = admix_models.populations(model)
         for (i, frac) in enumerate(admix_frac):
-            result += '{:s}: {:.2f}%'.format(populations[i], 100*frac) + '\n'
+            population_en, population_zh = populations[i]
+            if zh == False: # English
+                population = population_en
+            else: # Chinese
+                population = population_zh
+            result += '{:s}: {:.2f}%'.format(population, 100*frac) + '\n'
         result += '\n'
 
         # print out results
@@ -51,6 +61,7 @@ args = arguments()
 # set models for calculation
 all_models = admix_models.models()
 if (args.models is None):
+    print('No model specified, all available models will be used.\n')
     models = all_models
 else:
     models = args.models
@@ -58,7 +69,8 @@ else:
         if not m in all_models:
             print('Cannot find model ' + m + '!')
             exit()
+    print('Models: ' + ','.join(models) + '\n')
 
 raw_data_format = '23andme'
 
-admix_results(models, args.file, raw_data_format)
+admix_results(models, args.file, args.zh, raw_data_format)
