232f486344ad786ddc85880a3437e84ddb2c8c91
stevenliuyi <stevenliuyi@gmail.com>
2017-05-02 17:33:42 -0400

    add ability to save results as a file

--- admix/admix.py
+++ admix/admix.py
@@ -9,20 +9,41 @@ def arguments():
     # specify models
     parser.add_argument('-m', '--models',
                         nargs = '+',
-                        help = 'specify admixure models to use (default: all available models)')
+                        help = 'specify admixure models for calculation (default: all available models)')
+
+    # save as a file (set the default file name when no argument provided)
+    parser.add_argument('-f', '--file',
+                        nargs = '?',
+                        const = 'admixture_results.txt',
+                        help = 'save results as a file')
 
     return parser.parse_args()
 
 # print out admixure analysis results
-def admix_results(models, raw_data_format, raw_data_file=None):
+def admix_results(models, output_filename, raw_data_format, raw_data_file=None):
+    # write results to a file
+    if (output_filename is not None):
+        f = open(output_filename, 'w')
+
     for model in models:
-        print('Model: ' + model)
+        result = 'Model: ' + model + '\n'
         admix_frac = admix_fraction(model, raw_data_format, raw_data_file)
         populations = admix_models.populations(model)
         for (i, frac) in enumerate(admix_frac):
-            print('{:s}: {:.2f}%'.format(populations[i], 100*frac))
-        print('\n')
+            result += '{:s}: {:.2f}%'.format(populations[i], 100*frac) + '\n'
+        result += '\n'
 
+        # print out results
+        print(result)
+
+        # write results to file
+        if (output_filename is not None):
+            f.write(result)
+
+    # close file
+    if (output_filename is not None):
+        print('Results are written to ' + output_filename)
+        f.close()
 
 # arguments
 args = arguments()
@@ -40,4 +61,4 @@ else:
 
 raw_data_format = '23andme'
 
-admix_results(models, raw_data_format)
+admix_results(models, args.file, raw_data_format)
