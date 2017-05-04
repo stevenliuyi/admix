45b3db254f5abd64e3cdd4b078368dee66593b2a
stevenliuyi <stevenliuyi@gmail.com>
2017-05-03 23:11:30 -0400

    more PyPI classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='admix',
 
-    version='0.1.dev1',
+    version='0.1.dev2',
 
     description='An admixture analysis tool that supports raw data from 23andme, AncestryDNA, etc.',
     long_description=open('admix/README.rst','r').read(),
@@ -17,11 +17,16 @@ setup(
 
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Natural Language :: Chinese (Simplified)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
 
     keywords='bio DNA SNP ancestry admixture',
